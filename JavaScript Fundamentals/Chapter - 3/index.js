3247b063c7cdb2eae1ddb2b0772b70da41cad3a8
sonuchandrashekharpatel <sonuchandrashekharpatel@gmail.com>
2026-07-14 19:07:23 +0530

    Completed JavaScript challanges

--- JavaScript Fundamentals/Chapter - 3/index.js
+++ JavaScript Fundamentals/Chapter - 3/index.js
@@ -4,28 +4,154 @@
 
 
 /* Lesson 66: Sorting fruits */
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
+function putFruitIntoShelf() {
+    for(let i = 0; i< fruit.length; i++) {
+        if(fruit[i] === "🍎") {
+            appleShelf.textContent += "🍎"
+        } else {
+            orangeShelf.textContent += "🍊"
+        }
+    }
+}
+
+putFruitIntoShelf()
+
 
 
 /* Lesson 65: EmojiFighter */
+/* let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
 
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    
+    let firstFighter = fighters[Math.floor(Math.random() * fighters.length)]
+    let secondFighter = fighters[Math.floor(Math.random() * fighters.length)]
+    
+    stageEl.textContent = firstFighter + " vs " + secondFighter
+})
+ */
 
 /* Lesson 64: Rock papers scissors */
+/* 
+let hands = ["rock", "paper", "scissor"]
 
+// Create a function that returns a random item from the array
+
+function randomIndex () {
+    let randomIndex = Math.floor(Math.random() * hands.length)
+    return hands[randomIndex]
+}
+ */
 
 /* Lesson 63: Logical operators */
+/* 
+let dayOfMonth = 31
+let weekday = "Friday"
 
+// If it is Friday the 13th, log out this spooky face: 😱
+// Use the logical "AND operator" -> &&
+
+if(weekday === "Friday" && dayOfMonth === 13) {
+    console.log("😱")
+}
+ */
 
 /* Lesson 62: push, pop, unshift, shift challenge */
+/* let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
 
+// You need to help me fixup the largeCountries array so that 
+// China and Pakistan are added back into their respective places
+
+// Use push() & pop() and their counterparts unshift() & shift()
+// Google how to use unshift() and shift()
+
+largeCountries.pop()
+largeCountries.shift()
+largeCountries.unshift("China")
+largeCountries.push("Pakistan")
+console.log(largeCountries)
+ */
 
 /* Lesson 61: Loops and arrays */
+/* Use a for loop to log the following to the console:
 
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indinesia
+- Pakistan
+*/
+/* 
+let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+
+console.log('The 5 largest countries in the world:')
+for(let i=0; i< largeCountries.length; i++) {
+    console.log("- " + largeCountries[i])
+} */
 
 /* Lesson 60: if else */
+/* 
+let age = 15
 
+// less than 6 years old -> free
+// 6 to 17 years old     -> child discount
+// 18 to 26 years old    -> student discount
+// 27 to 66 years old    -> full price
+// over 66 years old     -> senior citizen discount
+
+// Create a conditional statement (if/else/else if) that logs out the discount
+// the passenger will get based upon the value of the age variable
+
+if(age < 6) {
+    console.log("free")
+} else if( age >= 6 && age <= 17) {
+    console.log("child discount")
+} else if( age >= 18 && age <= 26) {
+    console.log("student discount")
+}else if(age >= 27 && age <= 66){
+    console.log("full price")
+} else {
+    console.log("senior citizen discount")
+}
+*/
 
 /* Lesson 59: Objects and functions */
+/* 
+// Create a person object that contains three keys: name, age, and county.
+// Use yourself as an example to set the values for name, age, and country
 
+// Create a function, logData(), that uses the person object to create a
+// string in the following format: 
+// "Per is 35 years old and lives in Norway"
+
+// Call the logData() function to verify that it works
+
+let person = {
+    name: "Sonu",
+    age: 25,
+    country: "India"
+}
+
+function logData() {
+    let message = person.name + " is " + person.age + " years old and lives in " + person.country
+    console.log(message) 
+}
+
+logData()
+ */
 
 /* Lesson 58: JavaScript Challenges */
 
@@ -45,6 +171,7 @@ Recap:
 .   for loops
 .   The Math Object
 .   return statements
+
 */
 
 /* Lesson 55: AI for Coding Students */
