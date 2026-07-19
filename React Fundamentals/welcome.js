fe18a277fcc13315ca267de20c5167ee717d043b
sonuchandrashekharpatel <sonuchandrashekharpatel@gmail.com>
2026-07-19 18:40:12 +0530

    Adding welcome file that tell what we've covered

---
+++ React Fundamentals/welcome.js
@@ -0,0 +1,30 @@
+/* 
+Learn React:
+6 Project!
+    1. Fun facts about project
+    2. Travel Journal
+    3. Chef Claude
+    4. Meme Generator
+    5. Tenzies
+    6. Assembly Endgame
+
+2. Solo Project
+    1. 
+    2. 
+
+How to get the most from this course
+
+"The easy way is the hard way"
+    . Learn by doing
+    . Avoid "Tutorial Hell"
+    . You're not being graded
+    
+Make it stick
+    . Active Retrieval
+    . Spaced repetition
+    . Varied applications
+
+Teacher: Bob Ziroll
+
+
+*/
