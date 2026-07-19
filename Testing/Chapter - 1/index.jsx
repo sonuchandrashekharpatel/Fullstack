d5d8fa7e806882f4e56ccca4990656e67bce314a
sonuchandrashekharpatel <sonuchandrashekharpatel@gmail.com>
2026-07-19 18:47:53 +0530

    Setup of Testing Module

---
+++ Testing/Chapter - 1/index.jsx
@@ -0,0 +1,58 @@
+/* Chapter - 1: Testing */
+
+/* Lesson 12: Congratulations! */
+
+
+/* Lesson 11: Types of testing */
+
+
+/* Lesson 10: Accessibility testing with a side of TDD */
+
+
+/* Lesson 9: View code coverage */
+
+
+/* Lesson 8: Mock external services: Test */
+
+
+/* Lesson 7: Mock external services: Set up the tooling */
+
+
+/* Lesson 6: Test user interactions */
+
+
+/* Lesson 5: Practice, practice, and practice! */
+
+
+/* Lesson 4: Write your first test */
+
+
+/* Lesson 3: Set up the tooling */
+
+
+/* Lesson 2: What we will test: Meme Generator */
+
+
+/* Lesson 1: Welcome to the Testing Module */
+
+
+/* import lessonGenerator from "../../Aside/index.js"
+
+const chapterName = "Testing"
+const chapterNum = 1
+const lesson = [
+  "Welcome to the Testing Module",
+  "What we will test: Meme Generator",
+  "Set up the tooling",
+  "Write your first test",
+  "Practice, practice, and practice!",
+  "Test user interactions",
+  "Mock external services: Set up the tooling",
+  "Mock external services: Test",
+  "View code coverage",
+  "Accessibility testing with a side of TDD",
+  "Types of testing",
+  "Congratulations!"
+]
+lessonGenerator(chapterName, lesson, chapterNum) */ 
+
