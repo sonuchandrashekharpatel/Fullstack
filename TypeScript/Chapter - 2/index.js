b54206d71e150494a8d75986a35caca49e30c11a
sonuchandrashekharpatel <sonuchandrashekharpatel@gmail.com>
2026-08-15 08:43:57 +0530

    Scaffold Second Chapter of TypeScript :  TypeScript in React

---
+++ TypeScript/Chapter - 2/index.js
@@ -0,0 +1,94 @@
+/* Chapter - 2: TypeScript in React */
+
+/* Lesson 21: Solo project - Typed Tenzies */
+
+
+/* Lesson 20: Congrats! */
+
+
+/* Lesson 19: The Final Challenge */
+
+
+/* Lesson 18: Typing Function Props */
+
+
+/* Lesson 17: The WordLetters Component Challenge */
+
+
+/* Lesson 16: Element within an Element */
+
+
+/* Lesson 15: Component Props and Imported Types */
+
+
+/* Lesson 14: The AriaLiveStatus Component Challenge */
+
+
+/* Lesson 13: Custom Component Prop Types */
+
+
+/* Lesson 12: Typing Component Props */
+
+
+/* Lesson 11: JSX Element or null */
+
+
+/* Lesson 10: Typing React Components */
+
+
+/* Lesson 9: The New Setup */
+
+
+/* Lesson 8: Typing App.tsx functions */
+
+
+/* Lesson 7: Typing Derived Values and Arrow Functions */
+
+
+/* Lesson 6: Typing useState() */
+
+
+/* Lesson 5: TS Refresher - A new getRandomIndex() function */
+
+
+/* Lesson 4: TS Refresher - Functions */
+
+
+/* Lesson 3: TS Refresher - Basic & Custom Types */
+
+
+/* Lesson 2: Setting up the React Project */
+
+
+/* Lesson 1: Welcome to TypeScript and React! */
+
+
+/* 
+import lessonGenerator from "../../Aside/index.js"
+
+const chapterName = "TypeScript in React"
+const chapterNum = 2
+const lesson = [
+    "Welcome to TypeScript and React!",
+    "Setting up the React Project",
+    "TS Refresher - Basic & Custom Types",
+    "TS Refresher - Functions",
+    "TS Refresher - A new getRandomIndex() function",
+    "Typing useState()",
+    "Typing Derived Values and Arrow Functions",
+    "Typing App.tsx functions",
+    "The New Setup",
+    "Typing React Components",
+    "JSX Element or null",
+    "Typing Component Props",
+    "Custom Component Prop Types",
+    "The AriaLiveStatus Component Challenge",
+    "Component Props and Imported Types",
+    "Element within an Element",
+    "The WordLetters Component Challenge",
+    "Typing Function Props",
+    "The Final Challenge",
+    "Congrats!",
+    "Solo project - Typed Tenzies"
+]
+lessonGenerator(chapterName, lesson, chapterNum)  */
