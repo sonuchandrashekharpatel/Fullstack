06538fd352bccc628ea7df32f66aee33ed927acc
sonuchandrashekharpatel <sonuchandrashekharpatel@gmail.com>
2026-08-16 05:27:51 +0530

    Started TypeScript & Express Chapter of TypeScript

---
+++ TypeScript/Chapter - 3/index.ts
@@ -0,0 +1,109 @@
+/* Chapter - 3: TypeScript & Express */
+
+/* Lesson 24: Congratulations! */
+
+
+/* Lesson 23: Your own middleware */
+
+
+/* Lesson 22: Adding and typing middleware */
+
+
+/* Lesson 21: Separating concerns with controllers */
+
+
+/* Lesson 20: Separating concerns with Router */
+
+
+/* Lesson 19: Number query params */
+
+
+/* Lesson 18: Boolean query params */
+
+
+/* Lesson 17: An even more specific Request */
+
+
+/* Lesson 16: String query params */
+
+
+/* Lesson 15: Non-existent IDs */
+
+
+/* Lesson 14: A more specific Request */
+
+
+/* Lesson 13: A more specific Response */
+
+
+/* Lesson 12: Automating the server restart */
+
+
+/* Lesson 11: Typing the 404 catch-all */
+
+
+/* Lesson 10: cors and TypeScript */
+
+
+/* Lesson 9: Request and Response types */
+
+
+/* Lesson 8: Typing the real data */
+
+
+/* Lesson 7: Restarting the Express server */
+
+
+/* Lesson 6: Typing Express data */
+
+
+/* Lesson 5: Running an Express & TypeScript project */
+
+
+/* Lesson 4: Setting up an Express & TypeScript project */
+
+
+/* Lesson 3: Set up your environment */
+
+
+/* Lesson 2: Setting up a Express and TypeScript Environment */
+
+
+/* Lesson 1: Intro to Express & TypeScript */
+/* 
+
+*/
+
+
+/* 
+import lessonGenerator from "../../Aside/index.js"
+
+const chapterName = "TypeScript & Express"
+const chapterNum = 3
+const lesson = [
+    "Intro to Express & TypeScript",
+    "Setting up a Express and TypeScript Environment",
+    "Set up your environment",
+    "Setting up an Express & TypeScript project",
+    "Running an Express & TypeScript project",
+    "Typing Express data",
+    "Restarting the Express server",
+    "Typing the real data",
+    "Request and Response types",
+    "cors and TypeScript",
+    "Typing the 404 catch-all",
+    "Automating the server restart",
+    "A more specific Response",
+    "A more specific Request",
+    "Non-existent IDs",
+    "String query params",
+    "An even more specific Request",
+    "Boolean query params",
+    "Number query params",
+    "Separating concerns with Router",
+    "Separating concerns with controllers",
+    "Adding and typing middleware",
+    "Your own middleware",
+    "Congratulations!"
+]
+lessonGenerator(chapterName, lesson, chapterNum)  */
