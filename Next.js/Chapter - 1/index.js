2a3b1eca3e5371a0c9e80baa68bc98be21f9c29f
sonuchandrashekharpatel <sonuchandrashekharpatel@gmail.com>
2026-08-16 16:34:00 +0530

    Scaffold for Next.js

---
+++ Next.js/Chapter - 1/index.js
@@ -0,0 +1,114 @@
+/* Chapter - 1: Build a Next.js App */
+
+/* Lesson 26: Section 1 Recap */
+
+
+/* Lesson 25: Model Detail Page */
+
+
+/* Lesson 24: Dynamic Routes */
+
+
+/* Lesson 23: Challenge - Create the Models List Page */
+
+
+/* Lesson 22: Aside: Client vs. Server Components */
+
+
+/* Lesson 21: Aside: TypeScript Organization in PrintForge */
+
+
+/* Lesson 20: Challenge - Add Links to Navbar */
+
+
+/* Lesson 19: Links in Next.js */
+
+
+/* Lesson 18: Optimizing Images */
+
+
+/* Lesson 17: Optimizing Fonts */
+
+
+/* Lesson 16: Challenge - Add Header to PrintForge */
+
+
+/* Lesson 15: Layouts part 2 */
+
+
+/* Lesson 14: Layouts part 1 */
+
+
+/* Lesson 13: Nested Routes */
+
+
+/* Lesson 12: Challenge - PrintForge About Page */
+
+
+/* Lesson 11: Challenge - PrintForge Home Page */
+
+
+/* Lesson 10: PrintForge Challenge Setup */
+
+
+/* Lesson 9: create-next-app files walkthrough */
+
+
+/* Lesson 8: Scrimba's "Runner" */
+
+
+/* Lesson 7: Running create-next-app */
+
+
+/* Lesson 6: Understanding create-next-app */
+
+
+/* Lesson 5: File-based routing in Next.js */
+
+
+/* Lesson 4: Adding a new page to our site */
+
+
+/* Lesson 3: Challenge: create a new Next.js app from scratch! */
+
+
+/* Lesson 2: A Minimal Next.js Setup */
+
+
+/* Lesson 1: Bug Alert! */
+
+
+/* 
+import lessonGenerator from "../../Aside/index.js"
+
+const chapterName = "Build a Next.js App"
+const chapterNum = 1
+const lesson = [
+    "Bug Alert!",
+    "A Minimal Next.js Setup",
+    "Challenge: create a new Next.js app from scratch!",
+    "Adding a new page to our site",
+    "File-based routing in Next.js",
+    "Understanding create-next-app",
+    "Running create-next-app",
+    "Scrimba's \"Runner\"",
+    "create-next-app files walkthrough",
+    "PrintForge Challenge Setup",
+    "Challenge - PrintForge Home Page",
+    "Challenge - PrintForge About Page",
+    "Nested Routes",
+    "Layouts part 1",
+    "Layouts part 2",
+    "Challenge - Add Header to PrintForge",
+    "Optimizing Fonts",
+    "Optimizing Images",
+    "Links in Next.js",
+    "Challenge - Add Links to Navbar",
+    "Aside: TypeScript Organization in PrintForge",
+    "Aside: Client vs. Server Components",
+    "Challenge - Create the Models List Page",
+    "Dynamic Routes",
+    "Model Detail Page",
+    "Section 1 Recap"
+]
+lessonGenerator(chapterName, lesson, chapterNum)  */
